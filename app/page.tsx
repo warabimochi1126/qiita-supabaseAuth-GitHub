0195f29e7ee0a854947f146a5261b05da94daf10
warabimochi <satomi920003@yahoo.co.jp>
2024-04-04 23:52:57 +0900

    fix:コメントを追加

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const projectUrl = "https://ofloliwizljkltliktus.supabase.co";
+
+// 本来は秘匿すべき情報ですが、RLSポリシーを厳格に設定しているならば露出しても構いません。
 const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mbG9saXdpemxqa2x0bGlrdHVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyMzg1MDYsImV4cCI6MjAyNzgxNDUwNn0.V30UwX79wkyfWYaEF6hkdOneh38czL6c5n5UXBm9IlM";
 const supabase = createClient(projectUrl, apikey);
 
